a17040d744ef8196d0a65667c5730671919d44b1
Labib44 <mdlabib162@gmail.com>
2022-11-25 23:06:41 +0600

    Create booking post api

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const app= express();
-const port=process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middlewer
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 // Database
@@ -16,50 +16,72 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-try{
-    
-    const categorysCollection=client.db('best-buy').collection('categorys');
-    const productsCollection=client.db('best-buy').collection('products');
-// categorys
-    app.get('/categorysCollection',async(req, res)=>{
-        const query={};
-        const result=await categorysCollection.find(query).toArray();
-        res.send(result);
-    });
-// all product
-    app.get('/productsCollection',async(req, res)=>{
-        const query={};
-        const result=await productsCollection.find(query).toArray();
-        res.send(result);
-    });
+async function run() {
+    try {
 
-    app.get('/productsCollection/:id', async(req, res)=>{
-        const id=req.params.id;
-        const query={_id:ObjectId(id)}
-        const category=await categorysCollection.findOne(query);
-        const filter={category:category.category}
-        const result=await productsCollection.find(filter).toArray();
-        res.send(result);
-    });
-    app.get('/categorysCollection',async(req, res)=>{
-        const query={}
-        const result=await categorysCollection.find(query).toArray()
-        res.send(result);
-    })
-}
-finally{
+        const categorysCollection = client.db('best-buy').collection('categorys');
+        const productsCollection = client.db('best-buy').collection('products');
+        const bookingsCollection = client.db('best-buy').collection('bookings');
 
-}
+
+
+        // categorys
+        app.get('/categorysCollection', async (req, res) => {
+            const query = {};
+            const result = await categorysCollection.find(query).toArray();
+            res.send(result);
+        });
+        // all product
+        app.get('/productsCollection', async (req, res) => {
+            const query = {};
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/productsCollection/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const category = await categorysCollection.findOne(query);
+            const filter = { category: category.category }
+            const result = await productsCollection.find(filter).toArray();
+            res.send(result);
+        });
+        app.get('/categorysCollection', async (req, res) => {
+            const query = {}
+            const result = await categorysCollection.find(query).toArray()
+            res.send(result);
+        });
+
+        // bookings product
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        app.get('/bookings', async(req, res)=>{
+            const email=req.query.email;
+            const query={email:email};
+            const bookings=await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
+
+    }
+    finally {
+
+    }
 }
 run().catch(console.log);
 
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('Best Buy LTD server')
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Best Buy server on going ${port}`);
 })

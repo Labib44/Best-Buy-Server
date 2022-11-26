e01cb6f4a1df4dffb424b385522af7efd748a0ca
Labib44 <mdlabib162@gmail.com>
2022-11-27 00:51:21 +0600

    Create payment order api

--- index.js
+++ index.js
@@ -158,6 +158,14 @@ async function run() {
             const product=req.body;
             const result=await productsCollection.insertOne(product);
             res.send(result);
+        });
+
+        // payment booking product
+        app.get('/bookings/:id',async(req, res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+            const booking=await bookingsCollection.findOne(query);
+            res.send(booking);
         })
 
 
